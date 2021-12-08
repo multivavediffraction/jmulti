7f8c53149d71cba9f0dba6b4ef00f1dceba88a45
Serge Ivanov <lutvuk@gmail.com>
2021-12-08 21:31:34 +0300

    Updates due to new scalatest version.

--- calculator/src/test/scala/org/jmulti/calc/ComplexTest.scala
+++ calculator/src/test/scala/org/jmulti/calc/ComplexTest.scala
@@ -4,9 +4,11 @@ import org.scalatestplus.junit.JUnitRunner
 import org.junit.runner.RunWith
 
 import org.scalatest._
+import flatspec._
+import matchers._
 
 @RunWith(classOf[JUnitRunner])
-class ComplexTest extends FlatSpec with Matchers {
+class ComplexTest extends AnyFlatSpec with should.Matchers {
   "A complex exponent" should "produce same results as euler formula" in {
     val e1 = cexp(Complex(0, Math.PI))
 
